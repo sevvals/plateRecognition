38cb6ae0097de9428b4d8fe45e67f5ec885828cd
PC <sevvalakilli00@gmail.com>
2023-06-21 12:29:26 +0300

    UI

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import pytesseract
 import cv2
 from PIL import Image, ImageTk
 import tkinter as tk
+import re
 
 class Arayuz:
     def __init__(self, master):
         self.master = master
         master.title("Plaka Okuma Uygulaması")
+        master.geometry("1000x500")
 
-        self.upper_frame = tk.Frame(master, width=900, height=700)
-        self.upper_frame.pack(fill=tk.BOTH, expand=True)
+        self.video_frame = tk.Frame(master, width=700, height=500)
+        self.video_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
-        self.video_label = tk.Label(self.upper_frame, width=800, height=500)
-        self.video_label.pack(side=tk.LEFT)
+        self.video_label = tk.Label(self.video_frame)
+        self.video_label.pack()
 
-        self.right_frame = tk.Frame(master)
-        self.right_frame.pack(side=tk.LEFT, padx=20)
+        self.left_frame = tk.Frame(master, width=300, height=500)
+        self.left_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-        self.search_frame = tk.LabelFrame(self.right_frame, text="Arama", padx=10, pady=10)
+        self.search_frame = tk.LabelFrame(self.left_frame, text="Arama", padx=10, pady=10)
         self.search_frame.pack(pady=10)
 
         self.search_entry = tk.Entry(self.search_frame, width=20)
@@ -26,30 +28,34 @@ class Arayuz:
 
         self.search_entry.bind('<Return>', self.arama)
 
-        self.sonuc_label = tk.Label(self.right_frame, text="", padx=10, pady=10)
-        self.sonuc_label.pack()
-
-        self.plaka_label = tk.Label(self.right_frame, text="Plaka Listesi", padx=10, pady=10)
+        self.plaka_label = tk.Label(self.left_frame, text="Plaka Listesi", padx=10, pady=10)
         self.plaka_label.pack()
 
-        self.plaka_listesi = tk.Listbox(self.right_frame, width=30, height=15)
-        self.plaka_listesi.pack()
+        self.sonuc_label = tk.Label(self.left_frame, text="", padx=10, pady=10, anchor='w')
+        self.sonuc_label.pack()
 
-        self.boşluk_frame = tk.Frame(self.right_frame, height=50)
-        self.boşluk_frame.pack()
+        self.plaka_listesi_frame = tk.Frame(self.left_frame)
+        self.plaka_listesi_frame.pack(fill=tk.BOTH, expand=True)
+
+        self.scrollbar = tk.Scrollbar(self.plaka_listesi_frame)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.plaka_listesi = tk.Listbox(self.plaka_listesi_frame, width=30, height=15, yscrollcommand=self.scrollbar.set)
+        self.plaka_listesi.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        self.scrollbar.config(command=self.plaka_listesi.yview)
 
         self.plate_list = []
         self.plate_timestamps = []
         self.oynat()
 
-
     def oynat(self):
         model = YOLO('best.pt')
 
-        #Tesseract OCR
+        #tesseract OCR
         pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
-        cap = cv2.VideoCapture(r'C:\Users\PC\PycharmProjects\pythonProject9\VIDEO-2023-06-20-20-23-19_Trim_Trim.mp4')
+        cap = cv2.VideoCapture(r'C:\Users\PC\PycharmProjects\pythonProject9\carsvideo.mp4')
 
         def video_goster():
             ret, frame = cap.read()
@@ -72,16 +78,18 @@ class Arayuz:
 
                             blur = cv2.GaussianBlur(gray, (3, 3), 0)
 
-                            #ocr
-                            crop_img_text = pytesseract.image_to_string(blur, lang='eng')
+                            _, thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+                            crop_img_text = pytesseract.image_to_string(thresh, config='--psm 7')
+
                             crop_img_text = crop_img_text.strip()
 
-                            #plaka kontrol
-                            if len(crop_img_text.replace(" ", "")) >= 7 and len(crop_img_text.replace(" ", "")) <= 8:
-                                if crop_img_text.isalnum() or " " in crop_img_text:
-
-                                    if crop_img_text not in self.plate_list:
-                                        self.plate_list.append(crop_img_text)
+                            #plaka kontrolu yap
+                            clean_plaka = re.sub(r'[^a-zA-Z0-9]', '', crop_img_text)  #harf-rakam
+                            if len(clean_plaka) >= 7 and len(clean_plaka) <= 8:
+                                if clean_plaka.isalnum() and clean_plaka[0].isdigit():
+                                    if clean_plaka not in self.plate_list:
+                                        self.plate_list.append(clean_plaka)
                                         self.plate_timestamps.append(cap.get(cv2.CAP_PROP_POS_MSEC) / 1000)
 
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -91,12 +99,22 @@ class Arayuz:
                 self.video_label.configure(image=imgtk)
                 self.video_label.image = imgtk
 
-                #listeyi güncelle
+                #plaka listesi guncelle
                 self.plaka_listesi.delete(0, tk.END)
                 for plaka in self.plate_list:
-                    self.plaka_listesi.insert(tk.END, plaka)
+                    if len(plaka) == 7:
+                        formatted_plaka = f"{plaka[:2]} {plaka[2:4]} {plaka[4:]}"
+                    elif len(plaka) == 8:
+                        formatted_plaka = f"{plaka[:2]} {plaka[2:5]} {plaka[5:]}"
+                    else:
+                        formatted_plaka = plaka
+                    self.plaka_listesi.insert(tk.END, formatted_plaka)
 
-                self.master.after(1, video_goster)
+                if self.plaka_listesi.size() > 0:
+                    self.plaka_listesi.select_set(0)
+                    self.plaka_listesi.event_generate("<<ListboxSelect>>")
+
+            self.master.after(1, video_goster)
 
         video_goster()
 
@@ -105,12 +123,16 @@ class Arayuz:
         if aranan_plaka:
             self.plaka_listesi.selection_clear(0, tk.END)
 
+            aranan_plaka = re.sub(r'[^a-zA-Z0-9]', '', aranan_plaka)
+
             for i in range(self.plaka_listesi.size()):
-                plaka = self.plaka_listesi.get(i)
+                plaka = re.sub(r'\s', '', self.plaka_listesi.get(i))  #bosluk
                 if plaka.lower() == aranan_plaka.lower():
-                    self.plaka_listesi.selection_set(i)  #aranan plakayı seç
+                    self.plaka_listesi.selection_set(i)
                     timestamp = self.plate_timestamps[i] if i < len(self.plate_timestamps) else ""
-                    self.sonuc_label.config(text=f"Aranan plaka: {plaka} - Okunduğu dakika: {timestamp}")
+                    formatted_plaka = f"{plaka[:2]} {plaka[2:4]} {plaka[4:]}"  #plaka formatı
+                    self.sonuc_label.config(text=f"{formatted_plaka} - Okunduğu dakika: {timestamp:.2f}")
+                    self.sonuc_label.pack(after=self.plaka_label, anchor='w')
                     break
             else:
                 self.sonuc_label.config(text="Plaka bulunamadı")
